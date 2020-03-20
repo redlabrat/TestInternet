69541d5852a83660284b4999b70fb83c26577aa7
Alex Brych <redlabra@gmail.com>
2020-03-20 11:14:09 +0300

    Fixed error when saving same item in DB

--- app/src/main/java/by/brstu/redlabrat/testinternet/MovieDao.kt
+++ app/src/main/java/by/brstu/redlabrat/testinternet/MovieDao.kt
@@ -2,6 +2,7 @@ package by.brstu.redlabrat.testinternet
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 
 @Dao
@@ -9,7 +10,7 @@ interface MovieDao {
     @Insert
     fun addMovie(movie: SearchResultMovie)
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
     fun addMovies(movies: List<SearchResultMovie>)
 
     @Query("select * from movie_search")
